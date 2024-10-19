19463f38318159d673e26be2494d86d6cb3b5036
lukak85 <luka.kuzman85@gmail.com>
2024-10-19 12:25:00 +0200

    Fixed setup-transformer.sh for easier NVIDIA Triton SloBERTa deployment

--- sloberta/triton-inference-server/setup-transformers.sh
+++ sloberta/triton-inference-server/setup-transformers.sh
@@ -4,4 +4,4 @@ docker run --gpus=all \
     -p 8000:8000 -p 8001:8001 -p 8002:8002 \
     -v ${PWD}:/workspace/ \
     -v ${PWD}/model_repository:/model_repository tritonservertransformers:latest \
-    bash tritonserver --model-repository=/model_repository --log-verbose=1
+    bash -c "cd / && tritonserver --model-repository=/model_repository --log-verbose=1"
